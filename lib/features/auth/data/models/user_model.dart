9c09d7b1865b9120cba8b200b5380187d0abf24d
MostafaElzohirey <132284280+Mostafa-Elzohirey@users.noreply.github.com>
2025-02-01 21:15:42 +0200

    readme and comments

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:egy_tour/features/governments/data/models/land_mark_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hive/hive.dart';
 part 'user_model.g.dart';
