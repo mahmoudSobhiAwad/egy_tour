e8630d28321d4a2b399bf23b1f06fcd4c754c70a
MostafaElzohirey <132284280+Mostafa-Elzohirey@users.noreply.github.com>
2025-02-09 00:54:22 +0200

    location

---
+++ lib/features/home/presentation/views/location_view.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class LocationView extends StatefulWidget {
+  const LocationView(
+      {super.key, required this.latitude, required this.longitude});
+  final double latitude;
+  final double longitude;
+  @override
+  State<LocationView> createState() => _LocationViewState();
+}
+
+class _LocationViewState extends State<LocationView> {
+  List<Marker> markers = <Marker>[];
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+  @override
+  Widget build(BuildContext context) {
+    CameraPosition cairo = CameraPosition(
+      target: LatLng(widget.latitude, widget.longitude),
+      zoom: 12,
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Location'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: cairo,
+        mapType: MapType.normal,
+        markers: Set<Marker>.of(markers),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
+  }
+}
